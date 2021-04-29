3f861a5f6e6277818e4cb4e7d83164394317bfb0
Jerome Barbato <jerome@alaska.fr>
2021-04-29 10:57:27 +0200

    fix for composer 2

--- src/Composer/InstallerPlugin.php
+++ src/Composer/InstallerPlugin.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * User: Paul Coudeville <paul@metabolism.fr>
- */
 
 namespace Metabolism\WordpressCoreInstaller\Composer;
 
@@ -39,5 +36,33 @@ class InstallerPlugin implements PluginInterface
 
         $installer = new PackageInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
+    }
+
+    /**
+     * Remove any hooks from Composer
+     *
+     * This will be called when a plugin is deactivated before being
+     * uninstalled, but also before it gets upgraded to a new version
+     * so the old one can be deactivated and the new one activated.
+     *
+     * @param Composer $composer
+     * @param IOInterface $io
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement deactivate() method.
+    }
+
+    /**
+     * Prepare the plugin to be uninstalled
+     *
+     * This will be called after deactivate.
+     *
+     * @param Composer $composer
+     * @param IOInterface $io
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement uninstall() method.
     }
 }
